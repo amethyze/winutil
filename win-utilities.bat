b0b26d4a2d5c3fa72264b4f515fdcd53328e9ceb
Stevee <77215030+SteveYT77@users.noreply.github.com>
2021-08-31 20:50:22 -0500

    Update win-utilities.bat

--- win-utilities.bat
+++ win-utilities.bat
@@ -1,24 +1,21 @@
 @echo off
+set currver=0.7
 :start
 cd > %USERPROFILE%\AppData\Local\Temp\direc
-set /p direc= < direc
-set /p currver= 0.5
-del direc
+set /p direc= < %USERPROFILE%\AppData\Local\Temp\direc
+del %USERPROFILE%\AppData\Local\Temp\direc
 cd %USERPROFILE%\AppData\Local\Temp\
 echo Downloading dependencies...
+del newver.txt
 set verlink=https://raw.githubusercontent.com/SteveYT77/winutil/main/newver.txt 
 set verfile=newver.txt
-echo.
-echo %VERLINK%
-echo.
-echo %VERFILE%
 certutil -urlcache -split -f %verlink% %verfile%
 set /p latest= < newver.txt
 if %CURRVER%==%LATEST% (goto ignore) else (goto needupdate)
 
 
 :needupdate
-echo Do you want to update your version of WinUtil? Current version is v0.5 but the latest version is %LATEST%. [Y/N]
+echo Do you want to update your version of WinUtil? Current version is v%CURRVER% but the latest version is v%LATEST%. [Y/N]
 set /p update=""
 if /i %UPDATE%==y (goto update)
 if /i %UPDATE%==n (goto ignore)
@@ -32,17 +29,27 @@ goto start
 :update
 echo Downloading needed files... (Don't worry, this is NOT a virus!)
 cd %DIREC%
-set url=https://raw.githubusercontent.com/SteveYT77/winutil/main/winutil-en.txt
-set file=winutil-en.bat
+set url=https://raw.githubusercontent.com/SteveYT77/winutil/main/win-utilities.bat
+set file=win-utilities.txt
 certutil -urlcache -split -f %url% %file%
+set url=https://raw.githubusercontent.com/SteveYT77/winutil/main/winutil-en.txt
+set file=winutil-en.txt
+certutil -urlcache -split -f %url% %file%
+del winutil-en.bat
 rename winutil-en.txt winutil-en.bat
+echo rename %direc%/win-utilities.txt win-utilities.bat > %USERPROFILE%\AppData\Local\Temp\temp.txt
+echo start %direc%/win-utilities.bat > %USERPROFILE%\AppData\Local\Temp\temp2.txt
+type %USERPROFILE%\AppData\Local\Temp\temp2.txt>>%USERPROFILE%\AppData\Local\Temp\temp.txt
+del %USERPROFILE%\AppData\Local\Temp\temp2.txt
+rename %USERPROFILE%\AppData\Local\Temp\temp.txt temp.bat
 echo Done! Press any key to run.
 pause >nul
-call %USERPROFILE%\AppData\Local\Temp\winutil-en.bat
+start %USERPROFILE%\AppData\Local\Temp\temp.bat
 exit
 
 :ignore
-call %USERPROFILE%\AppData\Local\Temp\winutil-en.bat
+start winutil-en.bat
+exit
 
 :failsafe
 echo Whoops. That's not a valid option. Press any key to try again.
