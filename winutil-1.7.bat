9bc2a1d85f933f00dfc0425a907ba8be92559ae7
SteveeWasTaken <77215030+SteveeWasTaken@users.noreply.github.com>
2021-11-23 00:04:58 -0500

    Update 1.7

---
+++ winutil-1.7.bat
@@ -0,0 +1,636 @@
+@echo off
+set startupwinutilversion=1.7
+:initial
+if exist %USERPROFILE%\AppData\Local\WinUtil (goto initialfexist) else (goto initialfnotexist)
+
+:initialfexist
+if exist %USERPROFILE%\AppData\Local\WinUtil\settings.txt (goto initialconvert)
+if exist %USERPROFILE%\AppData\Local\WinUtil\settings.json (goto initialoexist) else (goto initialonotexist)
+
+:initialconvert
+(
+echo 0
+echo 0
+echo 1
+echo %STARTUPWINUTILVERSION%
+echo .
+echo .
+echo .
+echo .
+echo .
+)>%USERPROFILE%\AppData\Local\WinUtil\settings.json
+del %USERPROFILE%\AppData\Local\WinUtil\settings.txt
+cls
+goto initial
+
+:initialfnotexist
+mkdir %USERPROFILE%\AppData\Local\WinUtil
+goto initial
+
+:initialonotexist
+(
+echo 0
+echo 0
+echo 1
+echo %STARTUPWINUTILVERSION%
+echo .
+echo .
+echo .
+echo .
+echo .
+)>%USERPROFILE%\AppData\Local\WinUtil\settings.json
+goto initialoexist
+
+REM variables in order: start as admin, quick folder path, is it first time or not, version
+
+:initialoexist
+(
+set /p needadmin=
+set /p absolutelynothinguseful=
+set /p firsttime=
+set /p winutilversionold=
+set /p quickfolder1=
+set /p quickfolder2=
+set /p quickfolder3=
+set /p quickfolder4=
+set /p quickfolder5=
+)<%USERPROFILE%\AppData\Local\WinUtil\settings.json
+set winutilversion=%STARTUPWINUTILVERSION%
+(
+echo %NEEDADMIN%
+echo 0
+echo %FIRSTTIME%
+echo %WINUTILVERSION%
+echo %QUICKFOLDER1%
+echo %QUICKFOLDER2%
+echo %QUICKFOLDER3%
+echo %QUICKFOLDER4%
+echo %QUICKFOLDER5%
+)>%USERPROFILE%\AppData\Local\WinUtil\settings.json
+if %FIRSTTIME%==1 (goto firsttime) else (goto initialadmincheck)
+
+:firsttime
+echo Welcome to WinUtil. It seems like it's your
+echo first time running this program. In that case,
+echo I hope you love WinUtil. If you find any bugs
+echo or issues, please go to this program's GitHub
+echo and tell us more about it in there.
+echo.
+echo https://github.com/SteveYT77/winutil
+echo.
+echo Press anything to continue.
+pause >nul
+(
+echo %NEEDADMIN%
+echo 0
+echo 0
+echo %WINUTILVERSION%
+echo %QUICKFOLDER1%
+echo %QUICKFOLDER2%
+echo %QUICKFOLDER3%
+echo %QUICKFOLDER4%
+echo %QUICKFOLDER5%
+)>%USERPROFILE%\AppData\Local\WinUtil\settings.json
+goto startprev
+
+:initialadmincheck
+if %NEEDADMIN%==1 (goto initialadmincheckagain) else (goto startprev)
+
+:initialadmincheckagain
+openfiles >nul
+IF ERRORLEVEL 1 (goto initialadmin) else (goto startprev)
+
+:initialadmin
+powershell start -verb runas '%0' am_admin & exit /b
+
+:startprev
+goto start
+
+:start
+set util=blank
+set quick=blank
+set choice=blank
+set process=blank
+set pid=blank
+set usepid=blank
+set delete=blank
+set correct=blank
+set urlchoice=blank
+set setschoice=blank
+set delsetsask=blank
+set sspath=blank
+set ss=blank
+goto start2
+
+:start2
+cls
+title WinUtil v%WINUTILVERSION%
+echo  WinUtil - Windows MultiUtilities v1.7
+echo  Tool made by SteveeWasTaken                 
+echo.
+echo  [1] Restart File Explorer and Taskbar.                      
+echo.
+echo  [2] Kill any app that might be causing you problems.        
+echo.
+echo  [3] Open the Task Manager.                                  
+echo.
+echo  [4] Delete a file/folder of your choice.                    
+echo.
+echo  [5] Quick Access Shortcuts.
+echo.
+echo  [6] Log every existing file on a folder.                    
+echo.
+echo  [7] Faster Downloader                                       
+echo.
+echo  [8] [Deprecated.]
+echo.
+echo  [9] Super Shortcut Creator.                                 
+echo.
+echo  [S] Settings And Debugging Tools                            
+echo.
+echo  [A] About WinUtil                                           
+echo.
+echo  Press anything else to close WinUtil.            
+echo.
+set /p util=""
+cls
+if /i %UTIL%==x (goto bye)
+if /i %UTIL%==s (goto winutilsettings)
+if /i %UTIL%==a (goto aboutwinutil)
+goto w%UTIL%
+
+:winutilsettings
+title Settings
+cls
+echo WinUtil Settings
+echo.
+echo [1] Start WinUtil as administrator?
+echo.
+echo [2] [NONE]
+echo.
+echo [3] Change Quick Access Shortcuts
+echo.
+echo [S] Save settings.
+echo.
+echo [D] Delete settings.
+echo.
+echo [X] Go back to the main menu.
+set /p setschoice=""
+if /i %SETSCHOICE%==s (goto savesettings)
+if /i %SETSCHOICE%==x (goto start)
+if /i %SETSCHOICE%==d (goto deletesettings)
+goto settings%SETSCHOICE%
+
+:debugging
+set debuggingchoice=blank
+cls
+title Debug Mode
+echo [I] Variable Editor
+echo.
+echo [L] Enable/Disable Logging
+echo. 
+echo [X] Go back to the main menu.
+set /p debuggingchoice=""
+if /i %DEBUGGINGCHOICE%==I (goto input)
+if /i %DEBUGGINGCHOICE%==L (goto debug)
+if /i %DEBUGGINGCHOICE%==X (goto winutilsettings) else (goto fail)	
+cls
+
+:settings1
+echo.
+echo Would you like to start WinUtil as
+echo administrator? [Y/N]
+set /p usettings_1=""
+if /i %USETTINGS_1%==y (goto settings1yes)
+if /i %USETTINGS_1%==n (goto settings1no) else (goto fail)
+
+:settings1yes
+set NEEDADMIN=1
+echo Press anything to continue.
+pause>nul
+goto winutilsettings
+
+:settings1no
+set NEEDADMIN=0
+echo Press anything to continue.
+pause>nul
+goto winutilsettings
+
+:settings2
+echo You've chosen an option that doesn't
+echo exist. Press anything to go back.
+pause >nul
+goto winutilsettings
+
+:settings3
+echo What is the path to your Quick Access Shortcuts?
+set /p quickfolder1="Shortcut 1: "
+set /p quickfolder2="Shortcut 2: "
+set /p quickfolder3="Shortcut 3: "
+set /p quickfolder4="Shortcut 4: "
+set /p quickfolder5="Shortcut 5: "
+goto winutilsettings
+
+:savesettings
+(
+echo %NEEDADMIN%
+echo 0
+echo %FIRSTTIME%
+echo %WINUTILVERSIONCUR%
+echo %QUICKFOLDER1%
+echo %QUICKFOLDER2%
+echo %QUICKFOLDER3%
+echo %QUICKFOLDER4%
+echo %QUICKFOLDER5%
+)>%USERPROFILE%\AppData\Local\WinUtil\settings.json
+echo Settings saved.
+echo Press anything to go back to the menu.
+pause >nul
+goto start
+
+:deletesettings
+echo Are you sure you want to clear all of your
+echo settings? (REQUIRES WINUTIL RESTART) [Y/N]
+set /p delsetask=""
+if /i %DELSETASK%==y (goto deletesettingsy)
+if /i %DELSETASK%==n (goto winutilsettings) else (goto fail)
+
+:deletesettingsy
+del %USERPROFILE%\AppData\Local\WinUtil\settings.txt
+echo Cleared your settings. Press anything to restart
+echo WinUtil to apply changes.
+pause >nul
+exit
+
+:input
+title Variable Editor
+set vartoedit=blank
+set varsetas=blank	
+echo Welcome to the secret variable menu.
+echo What variable do you want to edit? (If you have
+echo access to this section, you should know the 
+echo variables you're editing.)
+set /p vartoedit=""
+echo What would you like to set the variable as?
+set /p varsetas=""
+echo Changing variables...
+@echo on
+set %VARTOEDIT%=%VARSETAS%
+@echo off
+echo Done.
+echo Sending you back to the main menu... Press any
+echo key to go back.
+pause >nul
+goto start2
+
+:debug
+echo Logging enabled.
+@echo on
+pause >nul
+goto start
+
+:debugoff
+set isdebug=false
+@echo off
+echo Debug disabled. Logging disabled.
+echo Press any key to go back to the main menu.
+pause >nul
+goto start
+
+:debugon
+set isdebug=true
+@echo on
+echo Debug enabled. Logging enabled.
+echo Press any key to go back to the main menu.
+pause >nul
+goto start
+
+:w1
+title Explorer reset
+echo Closing explorer...
+taskkill /f /im explorer.exe
+echo Opening explorer again... (If this window
+echo gets stuck, but you see your taskbar,
+echo close this window.)
+start explorer.exe
+echo Explorer restarted. Press anything to
+echo go back to the main menu.
+pause >nul
+goto start
+set /p choice=""
+if %CHOICE%==1 (goto start) else (goto bye)
+
+:w2
+title Program killer
+echo Do you know the process name?
+set /p choice="[Y/N] "
+if /i %CHOICE%==Y (goto 2yes)
+if /i %CHOICE%==N (goto 2no) else (goto fail)
+
+:2no
+tasklist
+echo Find the process in the list above.
+set /p process="Process name (include the .exe) "
+taskkill /f /im %PROCESS%
+echo The program should have closed. Else,
+echo check the message(s) above to make
+echo sure there weren't any errors. Press
+echo anything to go back to the main menu.
+pause >nul
+goto start
+
+:2yes
+set /p usepid="Use PID or use process name? [Pi/Pr] "
+if /i %USEPID%==pi (goto pid)
+if /i %USEPID%==pr (goto process) else (goto fail)
+
+:pid
+set /p pid="PID: "
+taskkill /f /pid %PID%
+echo The program should have closed. Else,
+echo check the message(s) above to make
+echo sure there weren't any errors. Press
+echo anything to go back to the main menu.
+pause >nul
+goto start
+
+:process
+set /p process="Process name (include the .exe) "
+taskkill /f /im %PROCESS%
+echo The program should have closed. Else,
+echo check the message(s) above to make
+echo sure there weren't any errors. Press
+echo anything to go back to the main menu.
+pause >nul
+goto start
+
+:w3
+title Task Manager
+echo Opening the Task Manager...
+start taskmgr.exe
+echo Task manager has opened. Press anything
+echo to go back to the main menu.
+pause >nul
+goto start
+
+:w4
+title File Eraser
+echo Directory/File to delete? (Include the
+echo  full route, example: C:\Users\user\myfile.txt)
+set /p delete=""
+echo.
+echo Are you sure the directory is correct?
+echo Syntax: %DELETE% [Y/N]
+set /p confirm=""
+cls
+if /i %DELETE%==Y (goto cont) else (goto no)
+
+:no
+cls
+echo You canceled the deletion. Press anything
+echo to go back to the main menu.
+pause >nul
+goto start
+
+:cont
+del %delete%
+echo Deletion should be done. Else, check the
+echo message(s) above to make sure there 
+echo weren't any errors. Press anything to go
+echo back to the main menu.
+set /p choice=""
+pause >nul
+goto start
+
+:w5
+if %QUICKFOLDER1%==. (goto w5error) else (goto w5continue)
+
+:w5error
+title ...
+echo You haven't set up your Shortcuts!
+echo Press anything to go back to the main menu.
+pause>nul
+goto start
+
+:w5continue
+title Quick Folder Shortcut Menu
+echo.
+echo [1] %QUICKFOLDER1%
+echo.
+echo [2] %QUICKFOLDER2%
+echo.
+echo [3] %QUICKFOLDER3%
+echo.
+echo [4] %QUICKFOLDER4%
+echo.
+echo [5] %QUICKFOLDER5%
+echo.
+echo [X] Back to the WinUtil menu
+set /p w5choice=""
+cls
+if /i %W5CHOICE%==x (goto start)
+goto w5-%W5CHOICE%
+
+:w5-1
+start "" "%QUICKFOLDER1%"
+goto w5
+
+:w5-2
+start "" "%QUICKFOLDER2%"
+goto w5
+
+:w5-3
+start "" "%QUICKFOLDER3%"
+goto w5
+
+:w5-4
+start "" "%QUICKFOLDER4%"
+goto w5
+
+:w5-5
+start "" "%QUICKFOLDER5%"
+goto w5
+
+:w6
+title Folder Logger
+cls
+set logfold=blank
+set logfoldsave=blank
+set logfoldtxt=blank
+echo Folder to log? (Insert full path.)
+set /p logfold=""
+echo Where to save logged text file? (Insert full path.)
+set /p logfoldsave=""
+echo Save as:
+set /p logfoldtxt=""
+FOR %%i IN (%LOGFOLD%\*.*) DO echo %%i >> %LOGFOLDSAVE%\%LOGFOLDTXT%.txt
+echo The file should have been created. Else, check
+echo the message(s) above to make sure there weren't
+echo any errors.
+echo Press anything to go back to the main menu.
+pause >nul
+goto start
+
+:w7
+title Fast Downloader
+cls
+set filechoice=blank
+echo Welcome to the Fast Downloader. Before
+echo continuing, you're gonna need a direct
+echo download link, if you don't use 
+echo a direct one specifically, you will
+echo have some problems.
+echo.
+echo Press [1] to continue with the process.
+echo Press [2] to get a quick tutorial on how to get
+echo a direct link.
+set /p filechoice=""
+if %FILECHOICE%==1 (goto 7cont)
+if %FILECHOICE%==2 (goto 7tut) else (goto fail)
+
+:7cont
+echo Direct link:
+set /p durl=""
+echo Save as (Include the full path including file extension):
+set /p dfile=""
+curl %DURL% --output %DFILE%
+echo Your download should be finished. Else, check the
+echo message(s) above to make sure there weren't any errors.
+echo Press anything to go back to the main menu.
+pause >nul
+goto start
+
+:7tut
+title Fast Downloader Tutorial
+cls
+set tutorialchoice=blank
+echo Welcome to the tutorial for direct links. What browser do
+echo you use? (The procedure is pretty similar for each one,
+echo but if you don't know much about what you're doing,
+echo it's recommended you follow the tutorial.)
+echo.
+echo [1] Mozilla Firefox
+echo.
+echo [2] Google Chrome / Brave Browser
+echo.
+echo [3] Opera / Opera GX
+set /p tutorialchoice=""
+if %TUTORIALCHOICE%==1 (goto 7firefox)
+if %TUTORIALCHOICE%==2 (goto 7google)
+if %TUTORIALCHOICE%==3 (goto 7opera)
+if %TUTORIALCHOICE%==4 (goto 7brave) else (goto fail)
+
+:7firefox
+cls
+echo 1. Open Firefox, then start a download via normal
+echo means. (Just download it as you would usually do.)
+echo 2. Right click on the file itself. A menu should 
+echo appear. Click "Copy Download Link".
+echo 2.5 Cancel the download as soon as you've got the
+echo download link. This is done to prevent massive network
+echo load.
+echo 3. Paste the link as the direct link in this program.
+echo.
+echo Press any key to go back to the quick downloader menu.
+pause >nul
+goto w7
+
+:7google
+cls
+echo Keep in mind that, while both Google Chrome and Brave
+echo browser look differently, they work the same way.
+echo 1. Open Google Chrome/Brave Browser, then start a
+echo download via normal means. (Just download it as 
+echo you would usually do.)
+echo 2. Click "Show All".
+echo 3. You should see a list of all of your downloads.
+echo Under the name of the one we're searching for, you
+echo should see a link. Right click it, and press "Copy
+echo link address."
+echo 3.5 Cancel the download as soon as you've got the
+echo download link. This is done to prevent massive network
+echo load.
+echo 4. Paste the link as the direct link in this program.
+echo.
+echo Press any key to go back to the quick downloader menu.
+pause >nul
+goto w7
+
+:7opera
+cls
+echo 1. Open Opera, then start a download via normal means.
+echo (Just download it as you would usually do.)
+echo 2. Click on the little download icon, then "Show More".
+echo 3. You should see a list of all of your downloads. 
+echo Under the name of the one we're searching for, you 
+echo should see a link. Right click it, and press 
+echo "Copy download address."
+echo 3.5 Cancel the download as soon as you've got the 
+echo download link. This is done to prevent massive network
+echo load.
+echo 4. Paste the link as the direct link in this program.
+echo.
+echo Press any key to go back to the quick downloader menu.
+pause >nul
+goto w7
+
+:w8
+title ...
+echo This option is now deprecated. Sorry for the
+echo inconvenience. Press anything to go back.
+pause >nul
+goto start
+
+:w9
+title Super Shortcut
+echo Welcome to the Super Shortcut Creator menu.
+echo This "Super" Shortcut is not a normal shortcut.
+echo It's a new shortcut that does NOT use the usual
+echo extension "lnk". In other words, this version of
+echo shortcuts does NOT conflict with any program or
+echo anything.
+set /p ss="Super Shortcut target path (Where the folder you want to create a Super Shortcut for is located. Example: C:\Windows\System32): 
+set /p sspath="Super Shortcut path (Where the new Super Shortcut will be, plus it's name. Example: C:\Users\user\Desktop\supershortcut): 
+mklink /D %SSPATH% %SS%
+echo Created your Super Shortcut. Press anything to go
+echo back to the main menu.
+pause >nul
+goto start
+
+:bye
+cls
+exit
+
+:aboutwinutil
+cls
+echo Hi. Thank you for using WinUtil. This program was
+echo made by SteveeWasTaken. The idea was born since one
+echo day, I was playing around with batch files. And
+echo I saw how much code there was, and I thought that
+echo there's people that don't know how to use this type of
+echo code correctly. So I decided I could help them, by
+echo making it easier for anyone to understand how it works,
+echo and so, the project started. This, of course, hasn't been
+echo easy, but it has been really fun to work on it. Again,
+echo thank you for using WinUtil. Press anything to go
+echo back to the main menu.
+pause >nul
+goto start
+
+:fail
+title  
+echo That wasn't an option. Press anything to go back.
+pause >nul
+goto start
+
+:failsafe
+title Oops...
+cls
+echo You find yourself in a strange situation... You have done something the program 
+echo wasn't expecting, and somehow ended up in a secret room. Please report what you
+echo did, to the developer, and he will do his best to fix it. Press any key to leave
+echo the program, and open the GitHub page.
+pause >nul
+start /max https://github.com/SteveYT77/winutil
+start /max https://raw.githubusercontent.com/SteveYT77/winutil/main/winutil-media/uhoh.txt
+exit
